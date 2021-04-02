1d0078793c89a1dde0271d44f5655ff3af916762
uyvaquoc123 <hieuzzai9@gmail.com>
2021-04-02 12:39:31 +0700

    first commit

---
+++ myscripts.js
@@ -0,0 +1,344 @@
+
+function myFunction() {
+    var element = document.getElementById("theMenu");
+    element.classList.toggle("menu-open");
+}
+function myFunction2() {
+    var element = document.getElementById("first");
+    element.classList.toggle("top-transform");
+}
+function myFunction3() {
+    var element = document.getElementById("second");
+    element.classList.toggle("middle-transform");
+}
+function myFunction4() {
+    var element = document.getElementById("third");
+    element.classList.toggle("bottom-transform");
+}
+function myFunction5() {
+    var element = document.getElementById("color-palate");
+    element.classList.toggle("popout-before");
+}
+function changeColor1() {
+    var maincard = document.getElementById("main-card").style.background = '#1F5694';
+    var skillcard = document.getElementById("skills-card").style.background = '#1F5694';
+    var button = document.getElementsByClassName("butt");
+    button[0].style.background = '#1F5694';
+    button[1].style.background = '#1F5694';
+    var slant = document.getElementsByClassName("slant")[0].style.borderBottom = '400px solid #1F5694';
+    var timelinedot = document.getElementsByClassName("timeline-dot");
+    for (i = 0; i < timelinedot.length; i++) {
+        timelinedot[i].style.background = '#1F5694';
+    }
+    var string = document.getElementsByClassName("string");
+    for (i = 0; i < string.length; i++) {
+        string[i].style.background = '#1F5694';
+    }
+    var verticalline = document.getElementsByClassName("timeline-vertical")[0].style.background = '#1F5694';
+    var cardcontentBorder = document.getElementsByClassName("timeline-content");
+    for (i = 0; i < cardcontentBorder.length; i++) {
+        cardcontentBorder[i].style.borderLeft = '5px solid #ff5722';
+    }
+    var skillbarGround = document.getElementsByClassName("skillbar");
+    for (i = 0; i < skillbarGround.length; i++) {
+        skillbarGround[i].style.background = '#1c4f87';
+    }
+    var plusCircle = document.getElementsByClassName("circle")[0].style.background = '#ff5722';
+
+    var threeStick = document.getElementsByTagName("i");
+    for (i = 0; i < threeStick.length; i++) {
+        threeStick[11].style.color = '#ff5722';
+        threeStick[12].style.color = '#ff5722';
+        threeStick[13].style.color = '#ff5722';
+    }
+    var logoFlat = document.getElementsByClassName("logo-flat")[0].style.background = '#1F5694';
+}
+
+
+function changeColor2() {
+    var maincard = document.getElementById("main-card").style.background = '#038175';
+    var skillcard = document.getElementById("skills-card").style.background = '#038175';
+    var button = document.getElementsByClassName("butt");
+    button[0].style.background = '#038175';
+    button[1].style.background = '#038175';
+    var slant = document.getElementsByClassName("slant")[0].style.borderBottom = '400px solid #038175';
+    var timelinedot = document.getElementsByClassName("timeline-dot");
+    for (i = 0; i < timelinedot.length; i++) {
+        timelinedot[i].style.background = '#038175';
+    }
+    var string = document.getElementsByClassName("string");
+    for (i = 0; i < string.length; i++) {
+        string[i].style.background = '#038175';
+    }
+    var verticalline = document.getElementsByClassName("timeline-vertical")[0].style.background = '#038175';
+    var cardcontentBorder = document.getElementsByClassName("timeline-content");
+    for (i = 0; i < cardcontentBorder.length; i++) {
+        cardcontentBorder[i].style.borderLeft = '5px solid #9C27B0';
+    }
+    var skillbarGround = document.getElementsByClassName("skillbar");
+    for (i = 0; i < skillbarGround.length; i++) {
+        skillbarGround[i].style.background = '#037267';
+    }
+    var plusCircle = document.getElementsByClassName("circle")[0].style.background = '#9C27B0';
+
+    var threeStick = document.getElementsByTagName("i");
+    for (i = 0; i < threeStick.length; i++) {
+        threeStick[11].style.color = '#9C27B0';
+        threeStick[12].style.color = '#9C27B0';
+        threeStick[13].style.color = '#9C27B0';
+    }
+    var logoFlat = document.getElementsByClassName("logo-flat")[0].style.background = '#038175';
+
+}
+
+function changeColor3() {
+    var maincard = document.getElementById("main-card").style.background = '#616161';
+    var skillcard = document.getElementById("skills-card").style.background = '#616161';
+    var button = document.getElementsByClassName("butt");
+    button[0].style.background = '#616161';
+    button[1].style.background = '#616161';
+    var slant = document.getElementsByClassName("slant")[0].style.borderBottom = '400px solid #616161';
+    var timelinedot = document.getElementsByClassName("timeline-dot");
+    for (i = 0; i < timelinedot.length; i++) {
+        timelinedot[i].style.background = '#616161';
+    }
+    var string = document.getElementsByClassName("string");
+    for (i = 0; i < string.length; i++) {
+        string[i].style.background = '#616161';
+    }
+    var verticalline = document.getElementsByClassName("timeline-vertical")[0].style.background = '#616161';
+    var cardcontentBorder = document.getElementsByClassName("timeline-content");
+    for (i = 0; i < cardcontentBorder.length; i++) {
+        cardcontentBorder[i].style.borderLeft = '5px solid #F44336';
+    }
+    var skillbarGround = document.getElementsByClassName("skillbar");
+    for (i = 0; i < skillbarGround.length; i++) {
+        skillbarGround[i].style.background = '#595959';
+    }
+    var plusCircle = document.getElementsByClassName("circle")[0].style.background = '#F44336';
+
+    var threeStick = document.getElementsByTagName("i");
+    for (i = 0; i < threeStick.length; i++) {
+        threeStick[11].style.color = '#F44336';
+        threeStick[12].style.color = '#F44336';
+        threeStick[13].style.color = '#F44336';
+    }
+    var logoFlat = document.getElementsByClassName("logo-flat")[0].style.background = '#616161';
+
+}
+
+
+function changeColor4() {
+    var maincard = document.getElementById("main-card").style.background = '#06A763';
+    var skillcard = document.getElementById("skills-card").style.background = '#06A763';
+    var button = document.getElementsByClassName("butt");
+    button[0].style.background = '#06A763';
+    button[1].style.background = '#06A763';
+    var slant = document.getElementsByClassName("slant")[0].style.borderBottom = '400px solid #06A763';
+    var timelinedot = document.getElementsByClassName("timeline-dot");
+    for (i = 0; i < timelinedot.length; i++) {
+        timelinedot[i].style.background = '#06A763';
+    }
+    var string = document.getElementsByClassName("string");
+    for (i = 0; i < string.length; i++) {
+        string[i].style.background = '#06A763';
+    }
+    var verticalline = document.getElementsByClassName("timeline-vertical")[0].style.background = '#06A763';
+    var cardcontentBorder = document.getElementsByClassName("timeline-content");
+    for (i = 0; i < cardcontentBorder.length; i++) {
+        cardcontentBorder[i].style.borderLeft = '5px solid #1976D2';
+    }
+    var skillbarGround = document.getElementsByClassName("skillbar");
+    for (i = 0; i < skillbarGround.length; i++) {
+        skillbarGround[i].style.background = '#05985a';
+    }
+    var plusCircle = document.getElementsByClassName("circle")[0].style.background = '#1976D2';
+
+    var threeStick = document.getElementsByTagName("i");
+    for (i = 0; i < threeStick.length; i++) {
+        threeStick[11].style.color = '#1976D2';
+        threeStick[12].style.color = '#1976D2';
+        threeStick[13].style.color = '#1976D2';
+    }
+    var logoFlat = document.getElementsByClassName("logo-flat")[0].style.background = '#06A763';
+
+}
+
+function changeColor5() {
+    var maincard = document.getElementById("main-card").style.background = '#5F1A89';
+    var skillcard = document.getElementById("skills-card").style.background = '#5F1A89';
+    var button = document.getElementsByClassName("butt");
+    button[0].style.background = '#5F1A89';
+    button[1].style.background = '#5F1A89';
+    var slant = document.getElementsByClassName("slant")[0].style.borderBottom = '400px solid #5F1A89';
+    var timelinedot = document.getElementsByClassName("timeline-dot");
+    for (i = 0; i < timelinedot.length; i++) {
+        timelinedot[i].style.background = '#5F1A89';
+    }
+    var string = document.getElementsByClassName("string");
+    for (i = 0; i < string.length; i++) {
+        string[i].style.background = '#5F1A89';
+    }
+    var verticalline = document.getElementsByClassName("timeline-vertical")[0].style.background = '#5F1A89';
+    var cardcontentBorder = document.getElementsByClassName("timeline-content");
+    for (i = 0; i < cardcontentBorder.length; i++) {
+        cardcontentBorder[i].style.borderLeft = '5px solid #E61F63';
+    }
+    var skillbarGround = document.getElementsByClassName("skillbar");
+    for (i = 0; i < skillbarGround.length; i++) {
+        skillbarGround[i].style.background = '#56187c';
+    }
+    var plusCircle = document.getElementsByClassName("circle")[0].style.background = '#E61F63';
+
+    var threeStick = document.getElementsByTagName("i");
+    for (i = 0; i < threeStick.length; i++) {
+        threeStick[11].style.color = '#E61F63';
+        threeStick[12].style.color = '#E61F63';
+        threeStick[13].style.color = '#E61F63';
+    }
+    var logoFlat = document.getElementsByClassName("logo-flat")[0].style.background = '#5F1A89';
+
+}
+
+function changeColor6() {
+    var maincard = document.getElementById("main-card").style.background = '#5D4037';
+    var skillcard = document.getElementById("skills-card").style.background = '#5D4037';
+    var button = document.getElementsByClassName("butt");
+    button[0].style.background = '#5D4037';
+    button[1].style.background = '#5D4037';
+    var slant = document.getElementsByClassName("slant")[0].style.borderBottom = '400px solid #5D4037';
+    var timelinedot = document.getElementsByClassName("timeline-dot");
+    for (i = 0; i < timelinedot.length; i++) {
+        timelinedot[i].style.background = '#5D4037';
+    }
+    var string = document.getElementsByClassName("string");
+    for (i = 0; i < string.length; i++) {
+        string[i].style.background = '#5D4037';
+    }
+    var verticalline = document.getElementsByClassName("timeline-vertical")[0].style.background = '#5D4037';
+    var cardcontentBorder = document.getElementsByClassName("timeline-content");
+    for (i = 0; i < cardcontentBorder.length; i++) {
+        cardcontentBorder[i].style.borderLeft = '5px solid #3F51B5';
+    }
+    var skillbarGround = document.getElementsByClassName("skillbar");
+    for (i = 0; i < skillbarGround.length; i++) {
+        skillbarGround[i].style.background = '#533931';
+    }
+    var plusCircle = document.getElementsByClassName("circle")[0].style.background = '#3F51B5';
+
+    var threeStick = document.getElementsByTagName("i");
+    for (i = 0; i < threeStick.length; i++) {
+        threeStick[11].style.color = '#3F51B5';
+        threeStick[12].style.color = '#3F51B5';
+        threeStick[13].style.color = '#3F51B5';
+    }
+    var logoFlat = document.getElementsByClassName("logo-flat")[0].style.background = '#5D4037';
+
+}
+
+function changeColor7() {
+    var maincard = document.getElementById("main-card").style.background = '#e42444';
+    var skillcard = document.getElementById("skills-card").style.background = '#e42444';
+    var button = document.getElementsByClassName("butt");
+    button[0].style.background = '#e42444';
+    button[1].style.background = '#e42444';
+    var slant = document.getElementsByClassName("slant")[0].style.borderBottom = '400px solid #e42444';
+    var timelinedot = document.getElementsByClassName("timeline-dot");
+    for (i = 0; i < timelinedot.length; i++) {
+        timelinedot[i].style.background = '#e42444';
+    }
+    var string = document.getElementsByClassName("string");
+    for (i = 0; i < string.length; i++) {
+        string[i].style.background = '#e42444';
+    }
+    var verticalline = document.getElementsByClassName("timeline-vertical")[0].style.background = '#e42444';
+    var cardcontentBorder = document.getElementsByClassName("timeline-content");
+    for (i = 0; i < cardcontentBorder.length; i++) {
+        cardcontentBorder[i].style.borderLeft = '5px solid #FF8F00';
+    }
+    var skillbarGround = document.getElementsByClassName("skillbar");
+    for (i = 0; i < skillbarGround.length; i++) {
+        skillbarGround[i].style.background = '#e52d20';
+    }
+    var plusCircle = document.getElementsByClassName("circle")[0].style.background = '#FF8F00';
+
+    var threeStick = document.getElementsByTagName("i");
+    for (i = 0; i < threeStick.length; i++) {
+        threeStick[11].style.color = '#FF8F00';
+        threeStick[12].style.color = '#FF8F00';
+        threeStick[13].style.color = '#FF8F00';
+    }
+    var logoFlat = document.getElementsByClassName("logo-flat")[0].style.background = '#e42444';
+
+}
+
+function changeColor8() {
+    var maincard = document.getElementById("main-card").style.background = '#cf2b7e';
+    var skillcard = document.getElementById("skills-card").style.background = '#cf2b7e';
+    var button = document.getElementsByClassName("butt");
+    button[0].style.background = '#cf2b7e';
+    button[1].style.background = '#cf2b7e';
+    var slant = document.getElementsByClassName("slant")[0].style.borderBottom = '400px solid #cf2b7e';
+    var timelinedot = document.getElementsByClassName("timeline-dot");
+    for (i = 0; i < timelinedot.length; i++) {
+        timelinedot[i].style.background = '#cf2b7e';
+    }
+    var string = document.getElementsByClassName("string");
+    for (i = 0; i < string.length; i++) {
+        string[i].style.background = '#cf2b7e';
+    }
+    var verticalline = document.getElementsByClassName("timeline-vertical")[0].style.background = '#cf2b7e';
+    var cardcontentBorder = document.getElementsByClassName("timeline-content");
+    for (i = 0; i < cardcontentBorder.length; i++) {
+        cardcontentBorder[i].style.borderLeft = '5px solid #9C27B0';
+    }
+    var skillbarGround = document.getElementsByClassName("skillbar");
+    for (i = 0; i < skillbarGround.length; i++) {
+        skillbarGround[i].style.background = '#ca195a';
+    }
+    var plusCircle = document.getElementsByClassName("circle")[0].style.background = '#9C27B0';
+
+    var threeStick = document.getElementsByTagName("i");
+    for (i = 0; i < threeStick.length; i++) {
+        threeStick[11].style.color = '#9C27B0';
+        threeStick[12].style.color = '#9C27B0';
+        threeStick[13].style.color = '#9C27B0';
+    }
+    var logoFlat = document.getElementsByClassName("logo-flat")[0].style.background = '#cf2b7e';
+
+}
+
+function hover(i) {
+    var colorChanged = document.getElementsByTagName("a");
+    var logoFlat = document.getElementsByClassName("logo-flat")[0].style.background;
+    colorChanged[i].style.color = '#1976D2';
+
+    if (logoFlat == 'rgb(31, 86, 148)') {
+        colorChanged[i].style.color = '#ff5722';
+    }
+    if (logoFlat == 'rgb(3, 129, 117)') {
+        colorChanged[i].style.color = '#9C27B0';
+    }
+    if (logoFlat == 'rgb(97, 97, 97)') {
+        colorChanged[i].style.color = '#F44336';
+    }
+    if (logoFlat == 'rgb(6, 167, 99)') {
+        colorChanged[i].style.color = '#1976D2';
+    }
+    if (logoFlat == 'rgb(95, 26, 137)') {
+        colorChanged[i].style.color = '#E61F63';
+    }
+    if (logoFlat == 'rgb(93, 64, 55') {
+        colorChanged[i].style.color = '#3F51B5';
+    }
+    if (logoFlat == 'rgb(228, 36, 68)') {
+        colorChanged[i].style.color = '#FF8F00';
+    }
+    if (logoFlat == 'rgb(207, 43, 126)') {
+        colorChanged[i].style.color = '#9C27B0';
+    }
+
+}
+function hoverout(i) {
+    document.getElementsByTagName("a")[i].style.color = '#616161';
+}
